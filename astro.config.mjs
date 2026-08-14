f529061b38f909332a28b7fc108bb9eafae74f81
phoenixcat01-windows <tararifan93@gmail.com>
2026-08-14 10:02:59 +0700

    delete project belajar-astrojs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
-import cloudflare from "@astrojs/cloudflare";
-
-export default defineConfig({
-    output: "server",
-    adapter: cloudflare()
-});
